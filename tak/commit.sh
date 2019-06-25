37755e4b591d4d8b2da0856be5d3a0925c369ec9
fuyutarow <fuyutarow@gmail.com>
2019-06-26 08:34:00 +0900

    Fix tak/commit

--- tak/commit.sh
+++ tak/commit.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# TODO: interactive arguments
-if [ $# -ne 2 ]; then
-  echo "$# arguments are required" 1>&2
-  exit 1
-fi
+read -p "What: " WHAT
+read -p "Where: " WHERE
 
-cat <<__EOT__
-(what)  $1
-(where) $2
-__EOT__
-
-echo -e "commit\t$1\t$2" >> record.log
-echo commit \"$1\" in $2
+echo -e "commit\t$WHAT\t$WHERE" >> record.log
+echo commit \"$WHAT\" in $WHERE
