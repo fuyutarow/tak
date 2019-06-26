6ca6345c2442ef8933f928082b788e1115c03c57
fuyutarow <fuyutarow@gmail.com>
2019-06-27 08:01:48 +0900

    Add tak/commit to tak/start

--- tak/start.sh
+++ tak/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo -e 'start\t'$(date "+%Y\t%m\t%d\t%R") >> record.log
 echo start at $(date "+%Y-%m-%dT%T")
+
+read -p "What: " WHAT
+read -p "Where: " WHERE
+
+echo -e "commit\t$WHAT\t$WHERE" >> record.log
+echo commit \"$WHAT\" in $WHERE
