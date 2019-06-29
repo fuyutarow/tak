176c9b8ec99bd7be7f511f59a497458f3dc785b6
fuyutarow <fuyutarow@gmail.com>
2019-06-30 04:51:03 +0900

    Fix. tak/start

--- tak/start.sh
+++ tak/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 TAK_DIR=$HOME/.tak
-echo -e 'start\t'$(date "+%Y\t%m\t%d\t%R") >> ${TAK_DIR%/}/record.log
-echo start at $(date "+%Y-%m-%dT%T")
 
 read -p "What: " WHAT
 read -p "Where: " WHERE
+
+echo -e 'start\t'$(date "+%Y\t%m\t%d\t%R") >> ${TAK_DIR%/}/record.log
+echo start at $(date "+%Y-%m-%dT%T")
 
 echo -e "commit\t$WHAT\t$WHERE" >> ${TAK_DIR%/}/record.log
 echo commit \"$WHAT\" in $WHERE
