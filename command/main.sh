ea941c275291f8e9aa342f4defc0c094b220b5c3
fuyutarow <fuyutarow@gmail.com>
2019-07-05 22:43:01 +0900

    Rename commands

--- command/main.sh
+++ command/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.0.5"
+VERSION="0.0.6"
 
 TAK_DIR=$HOME/.tak
 CommandDir=$TAK_DIR/command
@@ -9,17 +9,16 @@ usage() {
 cat<<EOS
 Usage: tak <command> [<args>]
 
-  begin       Log record When you start
-  start       Alias to start
-  end         Log recored what and when you end task
+  start       Log record When you start
+  stop        Log recored what and when you end task
+  edit        Edit record
+  status      Display tak status
+  makebranch  Make a new branch
+  checkout    Switch branch
+  branch      Show list of branches
   push        Push to your Google Calendar
   open        Open your Google Calendar with web browser
-  branch      Show list of branches
-  checkout    Switch branch
-  edit        Edit record
-  makebranch  Make a new branch
   issue       Open GitHub issue with web browser
-  status      Display tak status
   update      Update tak version
 EOS
 }
@@ -35,29 +34,19 @@ case "$Subcommand" in
         echo $VERSION
         exit 1
         ;;
-    's'|'status') bash $CommandDir/status.sh ;exit 1;;
-    'begin') bash $CommandDir/begin.sh ;exit 1;;
-    'start') bash $CommandDir/begin.sh ;exit 1;;
-    'end') bash $CommandDir/end.sh ;exit 1;;
+    'start') bash $CommandDir/start.sh ;exit 1;;
+    'stop') bash $CommandDir/stop.sh ;exit 1;;
+    'edit') bash $CommandDir/edit.sh ;exit 1;;
+    'status') bash $CommandDir/status.sh ;exit 1;;
+    'makebranch') bash $CommandDir/makebranch.sh ;exit 1;;
+    'checkout') bash $CommandDir/checkout.sh $@ ;exit 1;;
+    'branch') bash $CommandDir/branch.sh ;exit 1;;
     'push') bash $CommandDir/push.sh ;exit 1;;
     'opne') bash $CommandDir/open.sh ;exit 1;;
-    'b'|'branch') bash $CommandDir/branch.sh ;exit 1;;
-    'c'|'checkout') bash $CommandDir/checkout.sh $@ ;exit 1;;
-    'edit') bash $CommandDir/edit.sh ;exit 1;;
     'issue') bash $CommandDir/issue.sh ;exit 1;;
     'install') bash $TAK_DIR/scripts/install.sh ;exit 1;;
     'update') bash $TAK_DIR/scripts/update.sh ;exit 1;;
-    'makebranch') bash $CommandDir/makebranch.sh ;exit 1;;
-    -*)
-        echo "tak: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
-        exit 1
-        ;;
     *)
-        if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
-            #param=( ${param[@]} "$1" )
-            param+=( "$1" )
-            shift 1
-        fi
         ;;
 esac
 
@@ -72,3 +61,5 @@ while read line; do
     fi
   fi
 done < $HOME/.takconfig
+
+echo -e "tak: '$Subcommand' is not a tak command. See 'tak help'."
