2e9621b53456075430199472b3b72e6caa289f9c
fuyutarow <fuyutarow@gmail.com>
2019-07-16 01:33:26 +0900

    Fix checkout

--- command/branch.sh
+++ command/branch.sh
@@ -8,7 +8,7 @@ CurrentBranch=$(cat $TAK_DIR/config.tml | grep name | cut -f3 -d' ' | cut -f2 -d
 for f in ${TAK_DIR%/}/branches/*; do
   filename=$(echo "$f" | awk -F'/' '{print $NF}')
   BranchName=${filename%.*}
-  if [ $BranchName = $CurrentBranch ]; then
+  if [ "$BranchName" = "$CurrentBranch" ]; then
     GREEN='\033[0;32m'
     NC='\033[0m' # No Color]]
     echo -e "${GREEN}* ${BranchName}${NC}"
--- command/checkout.sh
+++ command/checkout.sh
@@ -10,8 +10,17 @@ if [ $# -ne 1 ]; then
   echo "❌ Branch name is required as argument." 1>&2
   exit 1
 fi
+
 Filename=$(ls ${TAK_DIR%/}/branches | grep $1 | head -1)
 TargetBranch="${Filename%.*}"
+
+
+if [ -n "$TargetBranch" ]; then
+  :
+else
+  echo "❌ $1: No such branch."
+  exit 1
+fi
 
 SrcDir=$TAK_DIR
 DstDir=${TAK_DIR%/}/branches/${CurrentBranch%/}
